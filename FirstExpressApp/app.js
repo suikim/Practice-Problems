87cd554447993abb887b28de0ed425bd93e0815e
suikim <susaninhakim@gmail.com>
2020-03-19 01:42:38 -0700

    Update app.js
    Learned route parameters and changing content of page based on parameters and get("*").

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -23,5 +23,23 @@ app.get('/dog', function(req, res){
 });
 
 
+//routes with different params in a pattern, also change content of page based on params
+app.get('/:animal/comments/:id/:title', function(req, res){
+	var subject = req.params.animal;
+	var topic = req.params.title;
+
+	res.send('Welcome to the ' + subject + ' page! Specifically, about ' + topic);
+	console.log("comments page");
+	//to print parameters in request
+	console.log(req.params);
+});
+
+
+// route for any url that doesn't match previous routes we made
+app.get('*', function(req, res){
+	res.send('Whoops, not valid url!');
+});
+
+
 //tells express to listen for requests and start server
 app.listen(3000, console.log("listening to port 3000"));
