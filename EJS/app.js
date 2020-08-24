f5887999fa2e6e938baf28e5f2dfcfaa423d38b6
suikim <susaninhakim@gmail.com>
2020-08-24 16:36:09 -0700

    Conditionals, Loops, Partials
    Practiced conditional and loops in ejs, using media and stylesheets with a 'public' folder and app.use(express.static("public")); , creating templates in "partials" to simplify repetitive code.

--- EJS/app.js
+++ EJS/app.js
@@ -3,11 +3,33 @@
 var express = require ("express");
 var app = express();
 
+// to use images/css stylesheets: static files in express must be inside directory 
+//    specified in static middleware (eg. 'public'), this line tells ejs to serve 
+//    public directory
+app.use(express.static( "public" ));
+
 // route for "/" page
 app.get("/", function(req, res){
 	//res.send("Welcome to the homepage");
 	res.render("home.ejs");
 });
+
+// route that uses params 'order'
+app.get("/menu", function(req, res){
+	var menu = [
+	{drink: "Coffee", cost: "$3"},
+	{drink: "Pour Over", cost: "$3.5"},
+	{drink: "French Press", cost: "$3.5"},
+	{drink: "Latte", cost: "$4.75"},
+	{drink: "Coffee Milk", cost: "$4.50"},
+	{drink: "Frappe", cost: "$5"},
+	{drink: "Mocha Pot", cost: "$4"},
+	{drink: "Coffee Beans (1lb)", cost: "$14"}
+	];
+
+	res.render("menu.ejs", {menu: menu})
+});
+
 
 // route that uses params 'order'
 app.get("/register/:order", function(req, res){
