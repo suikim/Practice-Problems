50f0d0b631d24fd68f98c35306f5103d0468fb3a
suikim <susaninhakim@gmail.com>
2020-08-28 15:58:30 -0700

    more post request practice
    new page to display order items, can add a drink and cost to order and updates cost of total cost

--- EJS/app.js
+++ EJS/app.js
@@ -8,6 +8,19 @@ var app = express();
 //    public directory
 app.use(express.static( "public" ));
 
+//var bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+// public array for youOrder to practice post request
+var totalOrder = [
+{drink: "Coffee", cost: 3},
+{drink: "Frappe", cost: 5},
+{drink: "coffee beans (1lb)", cost: 14}
+];
+
 // route for "/" page
 app.get("/", function(req, res){
 	//res.send("Welcome to the homepage");
@@ -36,6 +49,23 @@ app.get("/register/:order", function(req, res){
 	var order = req.params.order;
 	//res.send("I would like to have a " + order);
 	res.render("order.ejs", {printOrder: order});
+});
+
+
+// route for yourOrder 
+// lists items in totalOrder, can add new drinks and cost of drink into 
+//     totalOrder and updates total cost
+app.get("/yourOrder", function(req, res){
+	res.render("yourOrder.ejs", {order: totalOrder});
+	console.log(totalOrder);
+});
+
+// route for post request, add to totalOrder
+app.post("/addOrder", function(req, res){
+	var newDrink = req.body.drink;
+	var newCost = Number(req.body.cost);
+	totalOrder.push({drink: newDrink, cost: newCost});
+	res.redirect("/yourOrder");
 });
 
 
