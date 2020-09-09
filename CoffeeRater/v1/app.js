c8097f0c8f51bd4c4387159b7d9056f4d2347757
suikim <susaninhakim@gmail.com>
2020-09-09 16:31:31 -0700

    more styling
    styled allCafes page to use card components in a grid in bootstrap. changed number of cards in each row for small, medium, and large views

--- CoffeeRater/v1/app.js
+++ CoffeeRater/v1/app.js
@@ -9,13 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 // to use images/css stylesheets: static files in express must be inside directory 
 //    specified in static middleware (eg. 'public'), this line tells ejs to serve 
 //    public directory
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname, '/public'));
+app.use(express.static('/public'));
+
 
 //cafe array
 var cafeList = [
 	{name: "Starbucks", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQOF5bRSHAkAW2Xmsmlckd7ONYz4Gx6HIyUKA&usqp=CAU"},
 	{name: "Verve", image: "https://cdn.shopify.com/s/files/1/0035/9372/files/locations__pac_ave_900x.jpg?v=1578006217"},
 	{name: "Inkling", image: "https://static1.squarespace.com/static/55b9bec4e4b07881563239f7/55d5264ee4b035574f6b0cf8/57ec76c5bebafb2886c272a7/1475259057368/6Y9A8424.jpg?format=1500w"},
+	{name: "Peets", image: "https://nypost.com/wp-content/uploads/sites/2/2020/05/sized-peets.jpg?quality=80&strip=all"},
+	{name: "Sightglass Coffee", image: "https://studio-bba.com/wp-content/uploads/2010/01/sgc_6.jpg"},
+	{name: "Home Coffee Roasters", image: "https://cdn.shopify.com/s/files/1/0342/3925/0476/files/rsz_noriega_800x800_crop_center.jpg?v=1585187690"},
 	];
 
 //landing page
@@ -43,7 +48,7 @@ app.post("/allCafes", function(req, res){
 //restful convention
 app.get("/allCafes/new", function(req, res){
 	res.render("newCafe");
-});
+}); 
 
 
 app.listen(3000, console.log("listening to port 3000"));
