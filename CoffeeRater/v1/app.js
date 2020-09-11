f10634c6ea676605f2b92dbf9e9e3a62fbca74c8
suikim <susaninhakim@gmail.com>
2020-09-10 21:03:57 -0700

    more bootstrap
    added nav bar in header so each page has nav bar, did more styling on add cafe page.

--- CoffeeRater/v1/app.js
+++ CoffeeRater/v1/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 //    specified in static middleware (eg. 'public'), this line tells ejs to serve 
 //    public directory
 //app.use(express.static(__dirname, '/public'));
-app.use(express.static('/public'));
+app.use(express.static( "public" ));
 
 
 //cafe array
